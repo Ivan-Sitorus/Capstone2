3f6cb5d012d36d9346728572294ae85b2dfd65a3
Your Name <niohastungkoro28@gmail.com>
2026-07-26 12:27:20 +0700

    fix(piutang): add title/breadcrumb to ListPiutangs

--- app/Filament/Resources/PiutangResource/Pages/ListPiutangs.php
+++ app/Filament/Resources/PiutangResource/Pages/ListPiutangs.php
@@ -8,4 +8,14 @@ use Filament\Resources\Pages\ListRecords;
 class ListPiutangs extends ListRecords
 {
     protected static string $resource = PiutangResource::class;
+
+    public function getTitle(): string
+    {
+        return 'Piutang';
+    }
+
+    protected function getBreadcrumb(): ?string
+    {
+        return 'Piutang';
+    }
 }
